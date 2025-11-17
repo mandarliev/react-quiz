359ad4f83e6acc5706482be28856f505536a642e
Dimitar <dimitar.mandarliev@gmail.com>
2025-11-17 14:22:18 +0200

    feat: add StartScreen component and integrate loading/error states in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
 import { useEffect, useReducer } from "react";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -19,7 +22,9 @@ const reducer = (state, action) => {
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+
   useEffect(() => {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -30,8 +35,9 @@ function App() {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
